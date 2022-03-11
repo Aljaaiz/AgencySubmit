c56258e3cdaa6981c414a14d4d0f0104456262e9
Aljaaiz <aljaaiz1@gmail.com>
2022-03-11 20:59:52 +0400

    mobile menu retouch

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 let closeBtn = document.getElementById('close')
 let mobileMenu = document.getElementById('mobile-menu')
 let hamburger = document.getElementById('hamburger')
+let mobile_list = document.querySelectorAll('.mobile-list-item')
 
 hamburger.addEventListener('click', () => {
     mobileMenu.classList.add('active')
 })
 closeBtn.addEventListener('click', () => {
     mobileMenu.classList.remove('active')
+})
+mobile_list.forEach((item) => {
+    item.addEventListener('click', function () {
+        mobileMenu.classList.remove('active')
+    })
 })
